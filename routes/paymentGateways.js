3b86b398662a1f876145623b781ce0d67c180cc5
unknown <okwesimartins@gmail.com>
2026-01-14 13:48:54 +0100

    new

--- routes/paymentGateways.js
+++ routes/paymentGateways.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const paymentGatewayController = require('../controllers/paymentGatewayController');
 const { authenticate, authorize } = require('../middleware/auth');
+const { attachTenantDb } = require('../middleware/tenant');
+const { initializeTenantModels } = require('../middleware/models');
 
-// All routes require authentication
+// All routes require authentication and tenant DB
 router.use(authenticate);
+router.use(attachTenantDb);
+router.use(initializeTenantModels);
 
 // Get payment gateways
 router.get('/', paymentGatewayController.getPaymentGateways);
