ff686afb010b4d1ba260e8ad6f010d4d70ce7d37
unknown <okwesimartins@gmail.com>
2026-05-21 07:20:14 +0100

    new

--- app.js
+++ app.js
@@ -43,7 +43,20 @@ app.get('/connect/whatsapp/callback', (_req, res) => {
     var str = JSON.stringify(payload);
     try { var bc = new BroadcastChannel('wa_oauth_result'); bc.postMessage(str); bc.close(); } catch (_) {}
     try { if (window.opener) { window.opener.postMessage(str, window.location.origin); } } catch (_) {}
-    setTimeout(function () { window.close(); }, 1200);
+
+    // Use localStorage token presence to distinguish mobile vs desktop.
+    // window.opener is severed by Facebook's COOP headers even for desktop popups.
+    var token = '';
+    try { token = localStorage.getItem('wa_oauth_return_token') || ''; } catch (_) {}
+
+    if (token) {
+      try { localStorage.removeItem('wa_oauth_return_token'); } catch (_) {}
+      setTimeout(function () {
+        window.location.href = '/connect/whatsapp?token=' + encodeURIComponent(token);
+      }, 1500);
+    } else {
+      setTimeout(function () { window.close(); }, 1200);
+    }
   }
 
   if (err || !code) {
